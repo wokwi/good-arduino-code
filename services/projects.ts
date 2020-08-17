a96626a3cad16a04db4182db17e8b52427dd0f9a
Uri Shaked <uri@urishaked.com>
2020-08-18 00:13:44 +0300

    fix: remove `.cc` from valid code extensions
    Arduino does not recognize `.cc` as c++ files

--- services/projects.ts
+++ services/projects.ts
@@ -60,7 +60,7 @@ export async function getProjectText(id: string) {
 export async function getProjectCode(id: string) {
   const files = await fs.readdir(projectDir(id));
   const result: IProjectSourceFile[] = [];
-  const codeExtensions = ['.ino', '.c', '.h', '.cpp', '.cc'];
+  const codeExtensions = ['.ino', '.c', '.h', '.cpp'];
   for (const file of files) {
     const extension = extname(file.toLowerCase());
     if (codeExtensions.includes(extension)) {
