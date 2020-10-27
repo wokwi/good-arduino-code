d3d6274b119126742ba13bda9a4119b4257fcd84
Uri Shaked <uri@urishaked.com>
2020-10-27 23:30:33 +0200

    chore(deps): next 10.0.0

--- next.config.js
+++ next.config.js
@@ -1,30 +1,27 @@
 const withMdxEnhanced = require('next-mdx-enhanced');
-const withOptimizedImages = require('next-optimized-images');
 const readingTime = require('reading-time');
 
-module.exports = withOptimizedImages(
-  withMdxEnhanced({
-    fileExtensions: ['mdx'],
-    usesSrc: false,
-    extendFrontMatter: {
-      process: (mdxContent) => ({
-        readingTime: readingTime(mdxContent),
-      }),
-    },
-  })({
-    async redirects() {
-      return [
-        {
-          source: '/projects',
-          destination: `/`,
-          permanent: false,
-        },
-        {
-          source: '/guides',
-          destination: '/',
-          permanent: false,
-        },
-      ];
-    },
-  }),
-);
+module.exports = withMdxEnhanced({
+  fileExtensions: ['mdx'],
+  usesSrc: false,
+  extendFrontMatter: {
+    process: (mdxContent) => ({
+      readingTime: readingTime(mdxContent),
+    }),
+  },
+})({
+  async redirects() {
+    return [
+      {
+        source: '/projects',
+        destination: `/`,
+        permanent: false,
+      },
+      {
+        source: '/guides',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
+});
