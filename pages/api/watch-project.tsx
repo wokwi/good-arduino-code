8e43649ff5ff03ea816ee0d2c3c960b3be8dfa94
Uri Shaked <uri@urishaked.com>
2022-01-22 21:55:15 +0200

    chore(deps): next 10.2.0

--- pages/api/watch-project.tsx
+++ pages/api/watch-project.tsx
@@ -3,6 +3,10 @@ import { getStaticProps } from '../projects/[id]';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { project } = req.query;
-  const { props } = await getStaticProps({ params: { id: project as string } });
-  res.json(props);
+  const result = await getStaticProps({ params: { id: project as string } });
+  if ('props' in result) {
+    res.json(result.props);
+  } else {
+    res.json(result);
+  }
 };
